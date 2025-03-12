b7ae89299217b28cea1619b89462ac3a0d97585b
andrei1112111 <atishkin060705@gmail.com>
2025-03-12 09:06:27 +0000

    fix code

--- lab2/sub3/single.cpp
+++ lab2/sub3/single.cpp
@@ -2,115 +2,95 @@
 #include <vector>
 #include <cmath>
 #include <chrono>
-
+#include <omp.h>
+#include <iomanip>
 
 size_t N;
 size_t threads;
-double tau;
 
-
-void MatrixVectorProduct(const std::vector<double> &matrix, const std::vector<double> &vector, std::vector<double> &resultVector) {
-    #if defined(SCHEDULE_DYNAMIC)
-    #pragma omp parallel for num_threads(threads) schedule(dynamic)
-    #elif defined(SCHEDULE_GUIDED)
-    #pragma omp parallel for num_threads(threads) schedule(guided)
-    #elif defined(SCHEDULE_AUTO)
-    #pragma omp parallel for num_threads(threads) schedule(auto)
-    #else
-    #pragma omp parallel for num_threads(threads) schedule(static)
-    #endif
-    for (int i = 0; i < N; ++i) {
+void MatrixVectorProduct(const std::vector<double> &matrix, const std::vector<double> &vector,
+                         std::vector<double> &resultVector, int lb, int ub) {
+    for (int i = lb; i <= ub; ++i) {
         resultVector[i] = 0;
-        for (int j = 0; j < N; ++j) {
+        for (int j = 0; j < N; ++j)
             resultVector[i] += matrix[i * N + j] * vector[j];
-        }
     }
 }
 
-void VectorSubtraction(const std::vector<double> &vector0, const std::vector<double> &vector1, std::vector<double> &resultVector) {
-    #if defined(SCHEDULE_DYNAMIC)
-    #pragma omp parallel for num_threads(threads) schedule(dynamic)
-    #elif defined(SCHEDULE_GUIDED)
-    #pragma omp parallel for num_threads(threads) schedule(guided)
-    #elif defined(SCHEDULE_AUTO)
-    #pragma omp parallel for num_threads(threads) schedule(auto)
-    #else
-    #pragma omp parallel for num_threads(threads) schedule(static)
-    #endif
-    for (int i = 0; i < N; ++i) {
+void VectorSubtraction(const std::vector<double> &vector0, const std::vector<double> &vector1,
+                       std::vector<double> &resultVector, int lb, int ub) {
+    for (int i = lb; i <= ub; ++i)
         resultVector[i] = vector0[i] - vector1[i];
-    }
 }
 
-void ScalarVectorProduct(double scalar, const std::vector<double> &vector, std::vector<double> &resultVector) {
-    for (int i = 0; i < N; ++i) {
+void ScalarVectorProduct(double scalar, const std::vector<double> &vector, std::vector<double> &resultVector,
+                         int lb, int ub) {
+    for (int i = lb; i <= ub; ++i)
         resultVector[i] = scalar * vector[i];
-    }
 }
 
-double Norm(const std::vector<double> &vector) {
-    double norm = 0.0;
-
-    #if defined(SCHEDULE_DYNAMIC)
-    #pragma omp parallel for num_threads(threads) schedule(dynamic)
-    #elif defined(SCHEDULE_GUIDED)
-    #pragma omp parallel for num_threads(threads) schedule(guided)
-    #elif defined(SCHEDULE_AUTO)
-    #pragma omp parallel for num_threads(threads) schedule(auto)
-    #else
-    #pragma omp parallel for num_threads(threads) schedule(static)
-    #endif
-    for (int i = 0; i < N; ++i) {
-        norm += vector[i] * vector[i];
-    }
-
-    return sqrt(norm);
+double squaredNorm(const std::vector<double> &vector, int lb, int ub) {
+    double result{};
+    for (int i = lb; i <= ub; ++i)
+        result += vector[i] * vector[i];
+    return result;
 }
 
-void Algorithm(const std::vector<double> &A, const std::vector<double> &b, std::vector<double> &X) {
+void Algorithm(const std::vector<double> &A, const std::vector<double> &b, std::vector<double> &X, double tau) {
     std::vector<double> bufferVector(N);
-
-    while (true) {
-        MatrixVectorProduct(A, X, bufferVector);
-        VectorSubtraction(bufferVector, b, bufferVector);
-
-        if (Norm(bufferVector) < 0.00001 * Norm(b)) { break;}
-
-        ScalarVectorProduct(tau, bufferVector, bufferVector);
-        VectorSubtraction(X, bufferVector, X);
+    double numerator{}, denominator{};
+#pragma omp parallel num_threads(threads)
+    {
+        int nThreads = omp_get_num_threads();
+        int threadId = omp_get_thread_num();
+        int items_per_thread = N / nThreads;
+        int lb = threadId * items_per_thread;
+        int ub = (threadId == nThreads - 1) ? (N - 1) : (lb + items_per_thread - 1);
+        double numBuf{}, denomBuf{};
+        while (true) {
+            MatrixVectorProduct(A, X, bufferVector, lb, ub);
+            VectorSubtraction(bufferVector, b, bufferVector, lb, ub);
+            numBuf = squaredNorm(bufferVector, lb, ub);
+            denomBuf = squaredNorm(b, lb, ub);
+#pragma omp single
+            {
+                numerator = 0;
+                denominator = 0;
+            }
+#pragma omp atomic
+                numerator += numBuf;
+#pragma omp atomic
+                denominator += denomBuf;
+#pragma omp single
+            {
+                numerator = sqrt(numerator);
+                denominator = sqrt(denominator);
+            }
+            if (numerator < 0.00001 * denominator)
+                break;
+            ScalarVectorProduct(tau, bufferVector, bufferVector, lb, ub);
+            VectorSubtraction(X, bufferVector, X, lb, ub);
+        }
     }
 }
 
 int main(int argc, char **argv) {
     N = atoi(argv[1]);
-    tau = std::stod(argv[2]);
+    double tau = std::stod(argv[2]);
     threads = atoi(argv[3]);
 
     std::vector<double> A(N * N, 1);
+#pragma omp parallel for num_threads(threads)
+    for (int i = 0; i < N; ++i)
+        A[i * N + i] = 2;
+
     const std::vector<double> b(N, N + 1);
     std::vector<double> X(N, 0);
 
-    #if defined(SCHEDULE_DYNAMIC)
-    #pragma omp parallel for num_threads(threads) schedule(dynamic)
-    #elif defined(SCHEDULE_GUIDED)
-    #pragma omp parallel for num_threads(threads) schedule(guided)
-    #elif defined(SCHEDULE_AUTO)
-    #pragma omp parallel for num_threads(threads) schedule(auto)
-    #else
-    #pragma omp parallel for num_threads(threads) schedule(static)
-    #endif
-    for (int i = 0; i < N; ++i) {
-        A[i * N + i] = 2;
-    }
-
     const auto start{std::chrono::steady_clock::now()};
-
-    Algorithm(A, b, X);
-
+    Algorithm(A, b, X, tau);
     const auto end{std::chrono::steady_clock::now()};
     const std::chrono::duration<double> elapsed_seconds{end - start};
-
-    std::cout << "Elapsed time: " << elapsed_seconds.count() << " seconds." << std::endl;
-              
+    std::cout << "Elapsed time: " << std::fixed << std::setprecision(5) << elapsed_seconds.count() << std::endl;
     return 0;
 }
